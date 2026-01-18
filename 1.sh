220057367ee757e20d808e422fa6146f9e9bc076
SuyihQwQ <153289369+su-yihong@users.noreply.github.com>
2026-01-18 08:44:37 +0800

    2e4123

--- 1.sh
+++ 1.sh
@@ -4,4 +4,3 @@ echo "正在获取安装包"
 wget "https://gh-proxy.org/https://github.com/su-yihong/share/raw/refs/heads/main/todesk-bin-4.8.1.0-2-x86_64.pkg.tar.zst"
 echo "获取成功，开始安装"
 pkexec pacman -U "$DIR/todesk-bin-4.8.1.0-2-x86_64.pkg.tar.zst"
-todesk
